36cc76fca95061eb50a3ed6c90f83c8fc98c4c13
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2024-03-04 00:27:11 +0100

    layout edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,11 @@ export default function RootLayout({
       <MintbaseWalletContextProvider {...MintbaseWalletSetup}>
         <html lang="en">
           <body className={inter.className}>
+            <div className="min-h-screen">
               <Navbar />
-              <div className="min-h-screen">{children}</div>
+              {children}
               <Footer />
+            </div>
           </body>
         </html>
       </MintbaseWalletContextProvider>
