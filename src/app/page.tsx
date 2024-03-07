c24b24868c1d5b14d55c4fe102599d77b7f82e7b
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2024-03-07 01:35:58 +0100

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Howitworks from "@/components/homepage/Howitworks";
 import Join from "@/components/homepage/Join";
 import Nftcollections from "@/components/homepage/Nftcollections";
 import Topcreators from "@/components/homepage/Topcreators";
+import Shared from "@/components/homepage/Shared";
+import Productprev from "@/components/homepage/Productprev";
 
 export const metadata: Metadata = {
   title: "Partage",
@@ -15,7 +17,9 @@ export default function Home() {
     <>
       <Homeheader />
       <Howitworks />
+      <Shared />
       <Topcreators />
+      <Productprev />
       <Nftcollections />
       <Join />
     </>
